8bbfef973d046a45637bf8f278c5d063bf6a5bb8
Santa Knolli <olli.coding@proton.me>
2025-02-18 13:56:02 +0100

    fixed get_file_path to add correct format

--- ctax/paths.py
+++ ctax/paths.py
@@ -18,7 +18,8 @@ def convert_extension(
 
 def create_file_path(
     file_name: str | Path,
-    directory: str
+    directory: str,
+    format: str = 'parquet'
     ) -> Path:
     """
     Loop for changing filepath in case it already exists
@@ -26,12 +27,13 @@ def create_file_path(
     :param output_file: name of the file
     :param folder: subfolder in data directory
     """
+
     base_name = Path(file_name).stem
     file_path = DATA_DIR / directory/ file_name
 
     i = 1
     while file_path.exists():
-        file_path = DATA_DIR / directory / Path(base_name + f"_{i}.{directory}")
+        file_path = DATA_DIR / directory / Path(base_name + f"_{i}.{format}")
         i += 1
 
     return file_path
