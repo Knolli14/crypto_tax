f2c5d22412821ee06394ebd1730bfe832faae079
Santa Knolli <olli.coding@proton.me>
2025-02-26 14:26:22 +0100

    added list_arguments and check_same_legths

--- ctax/utils.py
+++ ctax/utils.py
@@ -12,3 +12,11 @@ def to_datetime(col: pd.Series) -> pd.Series:
 def list_arguments(arg: Any) -> list[Any]:
     """Converts a single argument to a list if it is not already a list."""
     return arg if isinstance(arg, list) else [arg]
+
+
+def check_same_lengths(args: list) -> bool:
+    """Checks if all arguments have the same length."""
+    same_lengths = len(set(map(len, args))) == 1
+    if not same_lengths:
+        raise ValueError("Arguments must have the same length.")
+    return True
