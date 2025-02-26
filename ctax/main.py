eb421b8a6a44bb4b737cd7b5390cbb4d97adf7b9
Santa Knolli <olli.coding@proton.me>
2025-02-26 14:24:56 +0100

    can now preprocess from cli

--- ctax/main.py
+++ ctax/main.py
@@ -1,9 +1,33 @@
-from ctax.preprocess.preprocessor import Preprocessor
+import argparse
+from ctax.preprocess import ppfuncs as pp
+
+def main():
+    funcs = {
+        "bitpanda": pp.preprocess_bitpanda_files,
+        "kucoin": pp.preprocess_kucoin_files,
+     #   "merge": pp.merge_files,
+    }
+
+    parser = argparse.ArgumentParser(description="Preprocess CEX data files.")
+    parser.add_argument("filenames", nargs="+", type=str, help="The filenames to process")
+    parser.add_argument("cex", type=str, help="The CEX (kucoin or bitpanda)")
+
+    args = parser.parse_args()
+
+    filenames = args.filenames
+    cex = args.cex
+
+    # Beispielaufruf einer Funktion aus ppfuncs
+    func = funcs.get(cex)
+
+    if func:
+        for file_name in filenames:
+            func(file_name)
+
+    else:
+        print(f"CEX '{cex}' is not supported. Choose from {funcs.keys()}")
+
+    return None
 
 if __name__ == "__main__":
-
-    filename = str(input("Enter the filename: ")).strip()
-    cex = str(input("Enter the CEX: ")).strip()
-
-    Preprocessor.preprocess_file(filename, cex)
-    print("File processed")
+    main()
